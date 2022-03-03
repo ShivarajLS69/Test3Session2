e0cd0d78090105e8d10b8817e78b2333f9b43cde
ShivarajLS6 <97090603+ShivarajLS69@users.noreply.github.com>
2022-03-03 09:10:45 +0000

    p7finl.c

--- p7final.c
+++ p7final.c
@@ -1,1 +1,30 @@
-
+#include<stdio.h>
+struct _fraction
+{
+int num,den;
+};
+typedef struct _fraction fraction;
+int gcd(int a, int b)
+{
+  int t;
+  while(b!=0)
+    {
+      t=b;
+      b=a%b;
+      a=t;
+    }
+  return a;
+}
+fraction input_fraction()
+{
+  fraction f;
+  printf("input a fraction\n");
+  scanf("%d%d",&f.num,&f.den);
+  return f;
+}
+int main()
+{
+  fraction f;
+  f=input_fraction();
+  printf("%d/%d\n",f.num,f.den);
+}
