74d1e04457575608625c5c81c776468353f0bfc8
ShivarajLS6 <97090603+ShivarajLS69@users.noreply.github.com>
2022-03-09 04:33:17 +0000

    p1final.c

--- p1final.c
+++ p1final.c
@@ -1,1 +1,23 @@
 #include <stdio.h>
+void input(int *num1,int *den1,int *num2,int *den2)
+{
+  printf("Enter any numbers\n");
+  scanf("%d%d%d%d",num1,den1,num2,den2);
+}
+void add(int num1,int den1,int num2,int den2,int *num3,int *den3)
+{
+  *num3= (num1*den2)+(num2*den1);
+  *den3=den1*den2;
+
+}
+void output(int num1, int den1, int num2, int den2, int num3, int den3)
+{
+  printf("%d/%d + %d/%d = %d/%d ",num1,den1,num2,den2,num3,den3);
+  }
+void main()
+{
+  int a,b,c,d,e,f;
+  input(&a,&b,&c,&d);
+  add(a,b,c,d,&e,&f);
+  output(a,b,c,d,e,f);
+}
